ebb91b0156fd4370f08061d8f08666095b8f5659
gdes <gauthier1.descamps@gmail.com>
2019-04-24 13:56:19 +0200

    initial commit

---
+++ main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"fmt"
+	"strings"
+    "io/ioutil"
+)
+
+func main() {
+    b, err := ioutil.ReadFile("file.txt") 
+    if err != nil {
+        fmt.Print(err)
+    }
+	s := strings.Fields(string(b))
+	for i:=0;i<len(s);i++ {
+		sep:="/"
+		res:=strings.Split(s[i],sep)
+		fmt.Println(res[len(res)-1])
+	}
+}
